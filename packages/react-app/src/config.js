aecae9949b4bccaa653113ad82afdbbab8aff512
Akasshhhh <akashisntreal@gmail.com>
2023-12-11 01:50:59 +0530

    initial commit

---
+++ packages/react-app/src/config.js
@@ -0,0 +1,9 @@
+
+export const ROUTER_ADDRESS = "0x25853B99eB088C9B4D5B4849D43AB364266dB354"; 
+
+export const DAPP_CONFIG = {
+  readOnlyChainId: 152,
+  readOnlyUrls: {
+    [152]: "https://rbn-gcp-australia-southeast1-a-0-b-v2.devnet.redbelly.network:8545",
+  },
+};
